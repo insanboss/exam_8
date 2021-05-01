119e6a1b13083751bfd6c57d577d78db1d4e7fc6
insan <insanboss@gmail.com>
2021-05-01 14:41:29 +0600

    * Реализовано добавление товара

--- source/webapp/views/products.py
+++ source/webapp/views/products.py
@@ -1,5 +1,8 @@
-from django.views.generic import ListView, DetailView
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views.generic import ListView, DetailView, CreateView
 
+from webapp.forms import ProductForm
 from webapp.models import Product
 
 
@@ -14,3 +17,11 @@ class ProductView(DetailView):
     model = Product
     context_object_name = 'product'
 
+
+class ProductCreate(CreateView):
+    model = Product
+    template_name = 'products/product_create.html'
+    form_class = ProductForm
+
+    def get_success_url(self):
+        return reverse('products:product_view', kwargs={'pk': self.object.pk})
