11fbd78f57fc94b8f5ab474caac47fc5a29d1974
insan <insanboss@gmail.com>
2021-05-01 18:54:33 +0600

    * Реализована страница пользователя

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from accounts.views import RegisterView
+from accounts.views import RegisterView, UserDetailView
 
 app_name = 'accounts'
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('accounts/login/', LoginView.as_view(), name='login'),
     path('accounts/logout/', LogoutView.as_view(), name='logout'),
     path('create/', RegisterView.as_view(), name='create'),
+    path('<int:pk>/', UserDetailView.as_view(), name='detail'),
 ]
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,8 +1,8 @@
-from django.contrib.auth import login
+from django.contrib.auth import login, get_user_model
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from .forms import MyUserCreationForm
 
 
@@ -23,3 +23,14 @@ class RegisterView(CreateView):
         if not next_url:
             next_url = reverse('products:index')
         return next_url
+
+
+class UserDetailView(DetailView):
+    model = get_user_model()
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
+
+    def get_context_data(self, **kwargs):
+        reviews = self.get_object().profile.all()
+        kwargs['reviews'] = reviews
+        return super().get_context_data(**kwargs)
